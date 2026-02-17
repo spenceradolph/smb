d594d5072b80a1168d709ff178d5c49356c60b3e
spenceradolph <spenceradolph@ymail.com>
2026-02-16 22:48:43 -0600

    more scaffolding

---
+++ Payload_Type/smb_agent/smb_agent/agent_functions/builder.py
@@ -0,0 +1,62 @@
+from mythic_container.PayloadBuilder import *
+from mythic_container.MythicCommandBase import *
+from mythic_container.MythicRPC import *
+
+
+class SMB(PayloadType):
+    name = "smb"
+    file_extension = ""
+    author = "Spencer Adolph"
+    supported_os = [
+        SupportedOS.Windows
+    ]
+    wrapper = False
+    wrapped_payloads = []
+    note = "This payload connects to smb."
+    supports_dynamic_loading = False
+    mythic_encrypts = False
+    translation_container = None
+    agent_path = pathlib.Path(".") / "smb_agent"
+    agent_icon_path = agent_path / "agent_functions" / "smb.svg"
+    agent_code_path = agent_path / "agent_code"
+    c2_profiles = []
+    build_parameters = [
+        BuildParameter(
+            name="host",
+            parameter_type=BuildParameterType.String,
+            default_value="127.0.0.1",
+            description="Host to connect to.",
+            required=True
+        ),
+        BuildParameter(
+            name="username",
+            parameter_type=BuildParameterType.String,
+            default_value="Administrator",
+            description="Username to use during authentication.",
+            required=True
+        ),
+        BuildParameter(
+            name="password",
+            parameter_type=BuildParameterType.String,
+            default_value="password",
+            description="Password to use during authentication.",
+            required=True
+        ),
+    ]
+
+    async def build(self) -> BuildResponse:
+        resp = BuildResponse(
+            status=BuildStatus.Success,
+        )
+        return resp
+    
+    async def on_new_callback(self, newCallback: PTOnNewCallbackAllData) -> PTOnNewCallbackResponse:
+        # create the smb connection in the background
+        payload_uuid = newCallback.Payload.UUID
+        connect_ip = self.get_parameter('host')
+        username = self.get_parameter('username')
+        password = self.get_parameter('password')
+        
+        # output, errors = await connect_to_smb(payload_uuid, username, password, connect_ip)
+
+        return PTOnNewCallbackResponse(AgentCallbackID=newCallback.Callback.AgentCallbackID, Success=True)
---
+++ Payload_Type/smb_agent/smb_agent/agent_functions/reconnect.py
@@ -0,0 +1,52 @@
+from mythic_container.MythicCommandBase import *
+from mythic_container.MythicRPC import *
+
+class ReconnectArguments(TaskArguments):
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line, **kwargs)
+        self.args = []
+
+    async def parse_arguments(self):
+        pass
+
+
+class ReconnectCommand(CommandBase):
+    cmd = "reconnect"
+    needs_admin = False
+    help_cmd = "reconnect"
+    description = "Reconnect the SMB session"
+    version = 1
+    author = "Spencer Adolph"
+    argument_class = ReconnectArguments
+    attackmapping = []
+
+    async def create_go_tasking(self, taskData: MythicCommandBase.PTTaskMessageAllData) -> MythicCommandBase.PTTaskCreateTaskingMessageResponse:
+        payload_uuid = taskData.Payload.UUID
+
+        for buildParam in taskData.BuildParameters:
+            if buildParam.Name == "host":
+                connect_ip = buildParam.Value
+            elif buildParam.Name == "username":
+                username = buildParam.Value
+            elif buildParam.Name == "password":
+                password = buildParam.Value
+        
+        # output, errors = await connect_to_smb(payload_uuid, username, connect_ip, connect_port)
+
+        await SendMythicRPCResponseCreate(MythicRPCResponseCreateMessage(
+            TaskID=taskData.Task.ID,
+            Response="".encode("UTF8"),
+        ))
+
+        response = MythicCommandBase.PTTaskCreateTaskingMessageResponse(
+            TaskID=taskData.Task.ID,
+            Success=True,
+            Completed=True,
+            Stdout="",
+            Stderr=""
+        )
+        return response
+
+    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
+        resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
+        return resp
